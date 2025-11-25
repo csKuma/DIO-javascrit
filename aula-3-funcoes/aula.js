c51e3f648bbd5c6ef37415c084d4da26f2445fe5
kuma <christopherstefano1@gmail.com>
2025-11-25 20:35:14 -0300

    exemplos de funcoes basicas

---
+++ aula-3-funcoes/aula.js
@@ -0,0 +1,12 @@
+
+meuNome('felipe')
+meuNome('marcos')
+ehDeMaiorIdade(11)
+ehDeMaiorIdade(18)
+function meuNome(nome){
+    console.log('meu nome é:',nome)
+}
+
+function ehDeMaiorIdade(idade){
+    console.log(idade>=18?true:false)
+}
